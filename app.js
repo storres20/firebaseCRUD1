1294dbb191d80a0834c739e9a78160f66742a465
LoreCha <lorecha2027@gmail.com>
2021-06-21 18:52:48 -0500

    prueba de Base de Datos

---
+++ app.js
@@ -0,0 +1,13 @@
+var db = firebase.firestore();
+
+db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then((docRef) => {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+    console.error("Error adding document: ", error);
+});
