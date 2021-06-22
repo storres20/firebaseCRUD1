51d9e6137a60e7572a59b2b511a4c510081c7dca
LoreCha <lorecha2027@gmail.com>
2021-06-21 19:01:51 -0500

    codigo para boton GUARDAR

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var db = firebase.firestore();
 
-db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then((docRef) => {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch((error) => {
-    console.error("Error adding document: ", error);
-});
+
+//Agregar el Usuario, cada vez que se da CLICK al boton GUARDAR
+function guardar(){
+    //Capturamos el valor de los elementos que tienen ID nombre, apellido y fecha; en otras palabras, capturamos los valores de los INPUT TEXT y lo asignamos a las variables nombre, apellido, fecha
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var fecha = document.getElementById('fecha').value;
+
+    db.collection("users").add({
+        first: nombre, //var nombre = document...
+        last: apellido, //var apellido = document...
+        born: fecha //var fecha = document...
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+}
