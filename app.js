f2f9d8c068a845ea369965ad026fb9f43e3ffcf5
LoreCha <lorecha2027@gmail.com>
2021-06-28 19:16:01 -0500

    DataTable se cambio a idioma Spanish

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ var filaEditada; //!para capturara la fila editada o actualizada
 
 var dataSet = new Array();
 
-//creamos constantes para los iconos editar y borrar
+//* creamos constantes para los iconos editar y borrar - start
 const iconoEditar = '<svg class="bi bi-pencil-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/><path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/></svg>';
 const iconoBorrar = '<svg class="bi bi-trash" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>';
-
+//* creamos constantes para los iconos editar y borrar - stop
 
 
 //*Inicializamos DataTable - end
@@ -111,11 +111,13 @@ db.collection("users").onSnapshot((querySnapshot) => {
             doc.id, doc.data().first, doc.data().last, doc.data().born
         ]);
 
+        //? end
+
     });
 
+    //?Llenado de la DataTable con datos - start
 
-    var tableId = '#table_id';
-    var tableObj = $(tableId).DataTable();
+    var tableObj = $('#table_id').DataTable();
 
     // clear first
     if(tableObj!=null){
@@ -125,7 +127,11 @@ db.collection("users").onSnapshot((querySnapshot) => {
 
     //Lleno la DataTable
     $(document).ready( function () {
-        $('#table_id').DataTable({
+        tableId = $('#table_id').DataTable({
+            //Se cambio lenguaje de Datatable a Spanish
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json"},
+
             pageLength : 5,
             lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']],
             data: dataSet,
@@ -137,12 +143,16 @@ db.collection("users").onSnapshot((querySnapshot) => {
                 },
                 {
                     targets: -1,
+                    //defaultContent: "<div class='wrapper text-center'><div class='btn-group'><button class='btnEditar btn btn-primary' data-toggle='tooltip' title='Editar'>"+iconoEditar+"</button><button class='btn btn-danger' data-toggle='tooltip' title='Borrar' onclick='btnBorrar()'>"+iconoBorrar+"</button></div></div>"
+
                     defaultContent: "<div class='wrapper text-center'><div class='btn-group'><button class='btnEditar btn btn-primary' data-toggle='tooltip' title='Editar'>"+iconoEditar+"</button><button class='btnBorrar btn btn-danger' data-toggle='tooltip' title='Borrar'>"+iconoBorrar+"</button></div></div>"
                 }
             ]
 
         });
     } );
+
+    //?Llenado de la DataTable con datos - start
 
 });
 //*Leer Datos, desde DB Firebase - end
@@ -196,6 +206,40 @@ function eliminar(id){
         }
       })
 }
+
+    //?Borrar Datos con DataTable - start
+
+    //function btnBorrar(){
+
+    //$('#table_id tbody').on('click', 'tr', function() {
+    /*$('#table_id tbody').on('click', '.btnBorrar', function() {
+        filaEliminada = $(this);
+        Swal.fire({
+        title: '¿Está seguro de eliminar el producto?',
+        text: "¡Está operación no se puede revertir!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Borrar'
+        }).then((result) => {
+        if (result.value) {
+            //let fila = $('#tabla_id').DataTable().fnGetData($(this).closest('tr'));
+            let fila = $('#table_id').DataTable().row( this ).data();
+            let idt = fila[0];
+
+            console.log(idt);
+            //db.ref(`productos/${id}`).remove()
+            Swal.fire('¡Eliminado!', 'El producto ha sido eliminado.','success')
+        }
+        })
+	}); */
+    //};
+
+    //?Borrar Datos con DataTable - end
+
+
+
 
 //*Borrar Datos, en la Tabla y en DB Firebase - end
 
